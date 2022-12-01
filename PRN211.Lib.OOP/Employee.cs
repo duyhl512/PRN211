3a2c48d084ee974aad470dc827633eb07dca84fd
duyhl512 <duyhlse130146@fpt.edu.vn>
2022-12-01 13:47:57 +0700

    update CRUD

--- PRN211.Lib.OOP/Employee.cs
+++ PRN211.Lib.OOP/Employee.cs
@@ -15,11 +15,35 @@
 
         public int Workhourly { get; set; }
 
-        public double Rate { get; set; }    
+        public double Rate { get; set; }
+
+
 
         /*
          Contructor
          */
 
+        public Employee()
+        {
+
+        }
+
+        public Employee( int id, string name, DateOnly dob, int workhourly, double rate)
+        {
+            Id = id;
+            Name = name;
+            Dob = dob;
+            Workhourly = workhourly;
+            Rate = rate;
+        }
+        // Hàm tính lương
+
+        public double GetSalary() => Workhourly * Rate;
+
+        public override string? ToString() => Display();
+        
+        // Hiển thị thông tin
+
+        private String Display() => $"Id: {Id}, Name: {Name}, Date of Birth: {Dob}, Salary: {GetSalary}";
     }
 }
---
+++ PRN211.Lib.OOP/ManageEmployee.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRN211.Lib.OOP
+{
+    public class ManageEmployee
+    {
+        private List<Employee> _employees;
+        public List<Employee> Employees { get => _employees;  }
+
+
+
+        public void AddEmployee(Employee e)
+        {
+            if(!Check(e)) _employees.Add(e);
+        }
+
+
+        public void UpdateEmployee(Employee e)
+        {
+            int index = GetIndexEmployee(e);
+            // Lấy vị trí trong list mà e tồn tại
+            if(index != -1) _employees[index] = e;
+            
+        }
+
+
+        public void ShowAllEmployee()
+        {
+            foreach(Employee e in _employees)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+
+
+        private int GetIndexEmployee(Employee e)
+        {
+            for(int i = 0; i < _employees.Count; i++)
+            {
+                if (_employees[i].Id == e.Id) return i;
+            }
+            return -1;
+        }
+
+
+        private bool Check(Employee e)
+        {
+            for (int i = 0; i < _employees.Count; i++)
+            {
+                if (_employees[i].Id == e.Id)
+                {
+                    return true;
+                }
+            }
+            return false;
+
+        }
+
+    }
+}
