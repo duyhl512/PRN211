4c69b06494f28b7861d645c5e411751a1cc70416
duyhl512 <duyhlse130146@fpt.edu.vn>
2022-12-01 13:12:27 +0700

    Add project files.

---
+++ PRN211.App.OOP/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+    Yêu cầu quản lý nhân viên gồm id, name, dob, workhourly, rate,. Thực hiện các chức năng sau
+    + Nhập vào một hoặc nhiều nhân viên
+    + Thay đổi thông tin nhân viên bất kì
+    + Remove một nhân viên bất kì
+    + Hiển thị toàn bộ nhân viên ra ngoài màn hình
+    + Tính và hiển thị tổng lương ra màn hình của một nhân viên bất kỳ theo id
+ */
+
+/*
+    - Tính trừu tượng ( Abstraction)
+    - 
+ */
---
+++ PRN211.Lib.OOP/Employee.cs
@@ -0,0 +1,25 @@
+﻿namespace PRN211.Lib.OOP
+{
+    public class Employee
+    {
+        /*
+         Field - data
+         */
+        private int _id;
+
+        public int Id { get => _id; set => _id = value; }
+
+        public String Name { get; set; }
+
+        public DateOnly Dob { get; set; }
+
+        public int Workhourly { get; set; }
+
+        public double Rate { get; set; }    
+
+        /*
+         Contructor
+         */
+
+    }
+}
