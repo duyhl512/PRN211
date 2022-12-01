a39486eeb1a7e781c263f8dbbdda86ba43ae4fa7
duyhl512 <97441713+duyhl512@users.noreply.github.com>
2022-12-01 14:42:15 +0700

    Add files via upload

---
+++ OOP/PRN211.Lib.OOP/Employee.cs
@@ -0,0 +1,48 @@
+﻿namespace PRN211.Lib.OOP
+{
+    public class Employee
+    {
+        /*
+         Field - data
+         */
+        private int _id;
+
+        public int Id { get => _id; set => _id = value; }
+
+        public String Name { get; set; }
+
+        public DateOnly Dob { get; set; }
+
+        public int Workhourly { get; set; }
+        
+        public double Rate { get; set; }
+
+
+
+        /*
+         Contructor
+         */
+
+        public Employee()
+        {
+        }
+
+        public Employee( int id, string name, DateOnly dob, int workhourly, double rate)
+        {
+            Id = id;
+            Name = name;
+            Dob = dob;
+            Workhourly = workhourly;
+            Rate = rate;
+        }
+        // Hàm tính lương
+
+        public double GetSalary() => Workhourly * Rate;
+        
+        public override string? ToString() => Display();
+
+        // Hiển thị thông tin
+
+        public String Display() => $"Id: {Id}, Name: {Name}, Date of Birth: {Dob}, Salary: {GetSalary()}";
+    }
+}
